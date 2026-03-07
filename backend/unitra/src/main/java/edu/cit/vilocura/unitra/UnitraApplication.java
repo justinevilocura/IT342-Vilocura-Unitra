1b05fe20fa7c49258cfe725be46dd764c443c258
Justine Vilocura <justinevilocura12@gmail.com>
2026-03-07 19:23:44 +0800

    setup(backend)

---
+++ backend/unitra/src/main/java/edu/cit/vilocura/unitra/UnitraApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.vilocura.unitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitraApplication.class, args);
+	}
+
+}
